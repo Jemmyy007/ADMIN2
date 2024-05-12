f2be74415ce98e4abfeddc6190bdee8de4bd4bde
Jemmyy007 <ilyavorona2004@mail.ru>
2024-05-12 17:26:01 +0300

    Админка3

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const PORT = 3000;
 const app = express();
 
 app.use(
-    cors,
+    // cors,
     bodyParser.json(),
     express.static(path.join(__dirname, 'public')),
     mainRoute,
--- routes/games.js
+++ routes/games.js
@@ -1,11 +1,30 @@
 // routes/games.js
 const gamesRouter = require("express").Router(); // Создали роутер
-const {sendAllGames, deleteGame, addGameController} = require("../controllers/games")
-const { getAllGames } = require("../middlewares/games")
+const {sendAllGames, addGameController, sendUpdatedGames} = require("../controllers/games")
+const { getAllGames, checkIsTitleInArray, updateGamesArray, updateGamesFile, findGameById, deleteGame } = require("../middlewares/games")
 
-gamesRouter.post("/games", getAllGames, addGameController)
-gamesRouter.get("/games", getAllGames, sendAllGames)
-gamesRouter.delete("/games/:id", getAllGames, deleteGame);
+gamesRouter.get(
+    "/games", 
+    getAllGames, 
+    sendAllGames)
+
+gamesRouter.post(
+    "/games", 
+    getAllGames, 
+    checkIsTitleInArray, 
+    updateGamesArray, 
+    updateGamesFile, 
+    addGameController)
+
+gamesRouter.delete(
+    "/games/:id", 
+    getAllGames, 
+    findGameById,
+    deleteGame, 
+    updateGamesFile,
+    sendUpdatedGames
+    );
 
 
 module.exports = gamesRouter;
+
